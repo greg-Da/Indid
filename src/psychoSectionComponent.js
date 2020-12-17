bcdb1397999dfc6fe8d9b5e755357744e0da1684
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-12-17 12:39:37 +0100

    fix padding

--- src/psychoSectionComponent.js
+++ src/psychoSectionComponent.js
@@ -44,9 +44,9 @@ const Psycho = (props) =>{
         <div className="second">
             <div className="section-psychology">
                 <div className="container">
-                    <div className="tile is-ancestor pt-5">
+                    <div className="tile is-ancestor">
                         <article className="tile is-child box mt-4 ">
-                            <p className="title text-center">Psychology Section</p>
+                            <p className="title text-center">Psychology Section vvhuujjjj</p>
                         </article>
                     </div>
                     <div className="d-flex cards">
