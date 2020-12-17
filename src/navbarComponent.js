2b10c17024ccae453a8a05d63ae0d82d765678de
yannyiim <55101971+yannyiim@users.noreply.github.com>
2020-12-17 14:51:33 +0100

    prject

--- src/navbarComponent.js
+++ src/navbarComponent.js
@@ -6,7 +6,9 @@ const NavBar = (props) => {
     return (
         <nav>
             <div className="nav-wrapper">
-                <img src="https://cdn.discordapp.com/attachments/755800455506034728/788775593503358996/indid.png"/>
+                <a href=''>
+                    <img src="https://cdn.discordapp.com/attachments/755800455506034728/788775593503358996/indid.png"/>
+                </a>
                 <ul id="nav-mobile" className="right hide-on-med-and-down">
                     <li> <Button/> </li>
                 </ul>
