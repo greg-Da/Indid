0b5f831d813b19df342c515e88cff45938ee823e
lutiku <lutiku.ndongala@edu.devinci.fr>
2020-12-17 10:56:38 +0100

    Create PsychoSectionComponent

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { TimelineLite } from "gsap/all";
 import Button from "./downloadBtn";
 import NavBar from "./navbarComponent";
+import Psycho from "./psychoSectionComponent";
 
 gsap.registerPlugin(ScrollTrigger);
 
 function App() {
 
   const [first_button_forward] = useState(new TimelineLite({paused:true}));
-  const [second_button_forward] = useState(new TimelineLite({paused:true}));
   const [third_button_forward] = useState(new TimelineLite({paused:true}));
   
   
@@ -49,39 +49,7 @@ function App() {
         
       }, []);
 
-  useEffect(() => {
 
-    second_button_forward.to(".App", {
-      duration: .9,
-      backgroundImage: 'url(https://cdn.discordapp.com/attachments/755800455506034728/788768540336586792/MOSHED-2020-12-16-15-3-51.gif)',
-      opacity: 1 })
-
-        .to(".App", 0.1,{
-          x: "+=10",
-          yoyo: true,
-          repeat: 11,
-        }, 0)
-
-        .to(".second", {
-          display: "none"
-        }, 0)
-
-        .to(".App",{
-          duration: .5,
-          backgroundImage: 'url(https://cdn.discordapp.com/attachments/755800429102759988/783715777953923102/decor-ecole.jpg)',
-          opacity: 1 },"+=1.5")
-
-        .from(".third", {
-          y:200,
-          duration: 1.5,
-          opacity: 0,
-        })
-
-        .to(".third", {
-          display: "block"
-        }, 0)
-
-  }, []);
 
   useEffect(() => {
   third_button_forward.to(".App", {
@@ -141,52 +109,8 @@ function App() {
           </div>
         </div>
 
-          <div className="second">
-            <div className="section-psychology">
-              <div className="container">
-                <div className="tile is-ancestor pt-5">
-                  <article className="tile is-child box mt-4 ">
-                    <p className="title text-center">Psychology Section</p>
-                  </article>
-                </div>
-                <div className="d-flex cards">
-                  <div className=" left d-flex">
-                    <div className=" top-left-image-text-column d-flex flex-column">
-                      <div className=" top-left-image-text-row d-flex">
-                        <div className="image-left">
-                          <img src="https://bulma.io/images/placeholders/480x480.png"/>
-                        </div>
-                        <div className="text-left card">
-                          <div className="card-body">
-                            <h5 className="card-title">Text</h5>
-                          </div>
-                        </div>
-                      </div>
-                      <div className="text-bottom-left card">
-                        <div className="card-body">
-                          <h5 className="card-title">Text</h5>
-
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                  <div id="second_right" className=" right d-flex flex-column">
-                    <div className="text-right card">
-                      <div className="card-body">
-                        <h5 className="card-title">Text</h5>
-
-                      </div>
-                    </div>
-                    <div className="image-right bg-light">
-                      <img src="https://bulma.io/images/placeholders/480x480.png"/>
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-            <button type="button" onClick={() => second_button_forward.play()}>Move forward</button>
-          </div>
+          {/*________ Partie 2 ______*/}
+            <Psycho/>
 
           <div className="third">
             <div id="title_section" className="container">
